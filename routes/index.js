46905c2b4183432f0d5b2aac5f8891e7b663e937
Zoneam <zoneam@gmail.com>
2022-05-04 15:03:27 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -15,15 +15,15 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : 'recipes',
-    failureRedirect : 'recipes'
+    successRedirect : '/recipes',
+    failureRedirect : '/recipes'
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout();
-  res.redirect('recipes');
+  res.redirect('/recipes');
 });
 
 module.exports = router;
