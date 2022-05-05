a1b7df1bfde101cde52d00d6def3ae6fb34defed
Zoneam <zoneam@gmail.com>
2022-05-05 09:29:19 -0700

    update

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,6 @@
 const myLinks = document.querySelectorAll(".my-cards");
-  
 if(myLinks.length){
-    myLinks.forEach(el=>{
+        myLinks.forEach(el=>{
             el.addEventListener("click", (e) => {
             e.target.previousElementSibling.hidden = false;
             e.target.hidden = true;
@@ -9,21 +8,14 @@ if(myLinks.length){
     })
 }
   
-
-// const saveBtn = document.querySelectorAll(".save-btn");
-  
-//   if(saveBtn.length){
-//       saveBtn.forEach(el=>{
-//               el.addEventListener("click", (e) => {
-             
-              
-//               e.target.disabled = true;
-              
-//               e.target.innerHTML  = 'Saved';
-//                e.preventDefault();
-//               return false;  
-              
-//           })
-//       })
-//   }
+const saveBtn = document.querySelectorAll(".save-btn");
+if(saveBtn.length){
+    saveBtn.forEach(el=>{
+            el.addEventListener("click", (e) => {
+            e.target.disabled = true;
+            e.target.innerHTML  = 'Saved';
+            e.target.closest('form').submit();              
+          })
+      })
+}
    
