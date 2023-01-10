0225b354d4f6867250e5a8963738ac5f48abcd63
Zoneam <zoneam@gmail.com>
2023-01-09 19:14:21 -0800

    more readable code try catch implementation

--- controllers/likes.js
+++ controllers/likes.js
@@ -5,14 +5,23 @@ module.exports = {
   };
 // Liking or unliking recipe
 async function like(req, res) {
-    Like.findOne({recipe: req.params.id}, async function(err,found){
-        if(!found.users.includes(req.user.id)){
-            found.users.push(req.user.id);
-            await found.save();
-        } else {
-            found.users.splice(found.users.indexOf(req.user.id),1)
-            await found.save();
-        }
-    })
-res.redirect('/recipes')
-}
+    try {
+      // Find the like object for the specified recipe
+      const like = await Like.findOne({ recipe: req.params.id });
+      if (!like.users.includes(req.user.id)) {
+        // If the logged-in user has not liked the recipe, add their ID to the 'users' array
+        like.users.push(req.user.id);
+        await like.save();
+      } else {
+        // If the logged-in user has already liked the recipe, remove their ID from the 'users' array
+        like.users.splice(like.users.indexOf(req.user.id), 1);
+        await like.save();
+      }
+      // Redirect to the 'recipes' page
+      res.redirect('/recipes');
+    } catch (error) {
+      // If an error occurred, send it back to the client
+      res.send(error);
+    }
+  }
+  
--- models/recipe.js
+++ models/recipe.js
@@ -1,28 +1,31 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const recipeSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   picture: {
-    type: String
+    type: String,
   },
   calories: {
-    type:Number
+    type: Number,
   },
   like: {
-      type: Schema.Types.ObjectId, ref: 'Like'
+    type: Schema.Types.ObjectId,
+    ref: 'Like',
   },
   link: {
-    type: String
+    type: String,
   },
   user: {
-    type: Schema.Types.ObjectId, ref: 'User'
-  }
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
 }, {
-  timestamps: true
+  timestamps: true,
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
